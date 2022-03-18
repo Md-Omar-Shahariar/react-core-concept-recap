d3996a62a6914639fea1cfd7cf548c7dfd29312d
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-03-18 19:19:25 +0600

    reacp aa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
+      <LoadPost></LoadPost>
       <District name="Dhaka" specialty="Bakorkhani"></District>
       <District name="Bramman Baria" specialty="Jhodda"></District>
       <District name="Comilla" specialty="Maynamoti"></District>
+    </div>
+  );
+}
+function LoadPost() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <h1>Post: {posts.length}</h1>
+      {posts.map((post) => (
+        <Post post={post}></Post>
+      ))}
+    </div>
+  );
+}
+function Post(props) {
+  const user = props.post;
+  return (
+    <div style={{ backgroundColor: "green", padding: "20px", margin: "20px" }}>
+      <h3>Name: {user.name}</h3>
+      <h5>ID: {user.id}</h5>
     </div>
   );
 }
